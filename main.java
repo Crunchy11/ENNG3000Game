02ea85c7b09c986caf9247dc6ccd237367f9af62
Nicholas <nicholas.elias@students.mq.edu.au>
2026-08-04 14:39:30 +1000

    basics of game window

--- main.java
+++ main.java
@@ -11,31 +11,31 @@ import java.awt.Font;
 
 public class main {
     public static void main(String[] args) {
-        // Run the GUI creation on the Event Dispatch Thread for thread safety
         SwingUtilities.invokeLater(() -> {
-            
             // 1. Create the JFrame (The Window)
             JFrame frame = new JFrame("My Application Window");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close app on 'X' click
             int width = 1200;
             int height = 800;
-            frame.setSize(width, height); // Set window dimensions
+            frame.setSize(width, height); 
             
-            // 2. Create the JPanel (The Canvas)
+            // 2. Create the JPanel 
             JPanel panel = new JPanel();
-            panel.setLayout(null); // disable layout manager
+            panel.setLayout(null); // disable layout manager allows for custom control of placement
             
             // 3. Create UI Components
             //score
             JLabel score = new JLabel("Score:");
             score.setBounds(width/2-50, 10, 100, 30); // x, y, width, height
             score.setFont(new Font("SansSerif", Font.BOLD, 30));
+
            //time
             JLabel time = new JLabel("Time:");
             time.setBounds(10, 10, 100, 30); // x, y, width, height
             time.setFont(new Font("SansSerif", Font.BOLD, 28));    
             //JButton button = new JButton("Click Me");
             
+            //Play Area
             JPanel playArea = new JPanel();
             playArea.setBounds(150, 100, 900, 550);   // x, y, width, height
             playArea.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
