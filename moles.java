d19800eca5510db27672f7cc9b9d703ec0cb1e17
Nicholas <nicholas.elias@students.mq.edu.au>
2026-08-06 14:06:24 +1000

    difficulty setting added

--- moles.java
+++ moles.java
@@ -1,33 +1,4 @@
-import javax.swing.JButton;
-
-public class moles extends JButton {
-    private boolean isUp = false;
-
-    public moles() {
-        setBounds(0, 0, 80, 80); // playingscreen will position it
-        setVisible(false); // hidden until popped up
-        addActionListener(e -> whack());
-    }
-
-    public void popUp() {
-        isUp = true;
-        setVisible(true);
-    }
-
-    public void hide() {
-        isUp = false;
-        setVisible(false);
-    }
-
-    public boolean isUp() {
-        return isUp;
-    }
-
-    private void whack() {
-        if (isUp) {
-            hide();
-            // notify score somehow — see below
-        }
-    }
+public class moles {
+ 
 }
 
